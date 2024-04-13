e8c7817070c06fcedce7c0ea976a38205f753391
Андрей <@jay_gatz>
2024-04-14 03:21:25 +0700

    Stage-2: refactor. improved null checking 'correctAnswersCount'; created separate file with 'data class Word'

--- src/main/kotlin/Vocabulary.kt
+++ src/main/kotlin/Vocabulary.kt
@@ -2,35 +2,26 @@ package org.example
 
 import java.io.File
 
-data class Word(
-    val text: String,
-    val translate: String,
-    val correctAnswersCount: Int = 0,
-)
-
 fun main() {
 
     val wordsFile = File("words.txt")
 
     val vocabulary: MutableList<Word> = mutableListOf()
 
-    wordsFile.forEachLine {line: String ->  
+    wordsFile.forEachLine { line: String ->
         val parts = line.split("|")
 
         if (parts.size == 3) {
             val word = Word(
                 text = parts[0],
                 translate = parts[1],
-                correctAnswersCount = parts.getOrNull(2)?.toInt() ?: 0,
+                correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0,
             )
-
             vocabulary.add(word)
         }
     }
 
-    wordsFile.appendText("\nКоличество правильных ответов: ${vocabulary.last().correctAnswersCount}")
-
-    vocabulary.forEach{
-        println("${it.text} - ${it.translate}. Количество правильных ответов - ${it.correctAnswersCount}")
+    vocabulary.forEach {
+        println("${it.text} - ${it.translate} (Количество правильных ответов - ${it.correctAnswersCount})")
     }
 }
